5854665c824cd553446cf18dd175ba6eb6ff3146
jiwoni1210 <jiwoni1210@naver.com>
2024-05-23 10:44:33 +0900

    refactor: home 리팩토링

--- escape-fe/ssafy-escape/src/components/home/Home.tsx
+++ escape-fe/ssafy-escape/src/components/home/Home.tsx
@@ -1,44 +1,44 @@
 "use client"
+
 import { useRef, useState } from "react"
-import * as S from "../../app/homeStyle"
-import Login from "../login/Login"
-import { Canvas } from "@react-three/fiber"
-import HomeRoom from "./HomeRoom"
 import { useRouter } from "next/navigation"
+import { Canvas } from "@react-three/fiber"
+import Login from "../login/Login"
+import HomeRoom from "./HomeRoom"
 import CameraMoveToPosition, {
   CameraMoveToPositionRef,
 } from "./CameraMoveToPosition"
 import HeaderNav from "../common/HeaderNav"
-import { Paytone_One } from "next/font/google"
 import useUserStore from "@/stores/UserStore"
+import { paytoneOne } from "@/styles/GoogleFont"
+import * as S from "../../app/homeStyle"
 
 interface HomeProps {
-  showText?: boolean
+  showText?: boolean // 배경만 쓸 건지 여부(showText가 false면 배경만 쓰겠다는 뜻)
 }
 
-const paytoneOne = Paytone_One({
-  subsets: ["latin"],
-  weight: "400",
-})
-
+// 첫 시작 페이지
 const Home = ({ showText = true }: HomeProps) => {
-  const router = useRouter()
-  const [isModelLoaded, setIsModelLoaded] = useState(false)
+  const [isModelLoaded, setIsModelLoaded] = useState<boolean>(false)
   const [isStartClicked, setIsStartClicked] = useState<boolean>(false)
   const pointerLockControlsRef = useRef<CameraMoveToPositionRef>(null)
+  const router = useRouter()
   const { isLogin } = useUserStore()
 
-  const onMoveClick = () => {
-    pointerLockControlsRef.current?.moveToPosition(3, 1, -7)
+  // START 버튼을 눌렀을 시
+  const handleStartClick = () => {
+    pointerLockControlsRef.current?.moveToPosition(3, 1, -7) // 카메라 이동
     const accessToken = sessionStorage.getItem("access_token")
     if (accessToken && isLogin) {
+      // 세션에 accessToken 유무, 로그인 유무 확인하여 메인페이지로 보냄
       router.push("/main")
     } else {
       setIsStartClicked(true)
     }
   }
 
-  const onBackClick = () => {
+  // 로그인 창에서 뒤로가기 버튼을 눌렀을 시
+  const handleBackClick = () => {
     pointerLockControlsRef.current?.moveToPosition(4, 3, -2)
     setIsStartClicked(false)
   }
@@ -61,26 +61,26 @@ const Home = ({ showText = true }: HomeProps) => {
         <HomeRoom onLoaded={setIsModelLoaded} />
         <CameraMoveToPosition ref={pointerLockControlsRef} />
       </Canvas>
-      {!showText ? <HeaderNav /> : null}
-      {isModelLoaded ? (
+      {!showText && <HeaderNav />}
+      {isModelLoaded && (
         <div>
           {!isStartClicked && showText ? (
-            <>
+            <div>
               <S.TitleText className={paytoneOne.className}>
                 Cyber Escape
               </S.TitleText>
               <S.StartButtton
                 className={paytoneOne.className}
-                onClick={() => onMoveClick()}
+                onClick={handleStartClick}
               >
                 START
               </S.StartButtton>
-            </>
+            </div>
           ) : isStartClicked && showText ? (
-            <Login handleLoginback={onBackClick} />
+            <Login handleLoginback={handleBackClick} />
           ) : null}
         </div>
-      ) : null}
+      )}
     </div>
   )
 }
--- escape-fe/ssafy-escape/src/components/home/HomeRoom.tsx
+++ escape-fe/ssafy-escape/src/components/home/HomeRoom.tsx
@@ -1,31 +1,26 @@
-import { useGLTF } from "@react-three/drei"
 import { useEffect } from "react"
+import { useGLTF } from "@react-three/drei"
 import { Mesh } from "three"
 
-interface RoomProps {
-  onLoaded: (isLoaded: boolean) => void
-}
-
+// 배경이 되는 3D방 모델
 const HomeRoom = ({ onLoaded }: RoomProps) => {
   const gltf = useGLTF(
     process.env.NEXT_PUBLIC_IMAGE_URL + "/glb/home_Room.glb",
     true,
   )
 
+  // 모델이 완전히 load 된 후 나머지 요소들 출력
   useEffect(() => {
     if (gltf) {
       onLoaded(true)
+      gltf.scene.traverse((child) => {
+        if (child instanceof Mesh) {
+          child.castShadow = true
+          child.receiveShadow = true
+        }
+      })
     }
   }, [gltf, onLoaded])
-
-  useEffect(() => {
-    gltf.scene.traverse((child) => {
-      if (child instanceof Mesh) {
-        child.castShadow = true
-        child.receiveShadow = true
-      }
-    })
-  }, [gltf])
 
   return <primitive object={gltf.scene} scale={1} />
 }
